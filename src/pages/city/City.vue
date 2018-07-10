3e3244d8c3febe5bc611985c9dcff99e526bfb32
onReadyL <1169959173@qq.com>
2018-07-10 13:39:34 +0800

    添加better-scroll滑动插件，编写城市列表以及右侧字母排序

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,17 +2,23 @@
 <div>
   <city-header></city-header>
   <city-search></city-search>
+  <city-list></city-list>
+  <city-alphabet></city-alphabet>
 </div>
 </template>
 
 <script>
 import CityHeader from './components/Header.vue'
 import CitySearch from './components/CitySearch.vue'
+import CityList from './components/CityList.vue'
+import CityAlphabet from './components/Alphabet.vue'
 export default {
   name: 'City',
   components: {
     CityHeader: CityHeader,
-    CitySearch: CitySearch
+    CitySearch: CitySearch,
+    CityList: CityList,
+    CityAlphabet: CityAlphabet
   }
 }
 </script>
---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul class="letter-list">
+    <li class="letter-list-item">A</li>
+    <li class="letter-list-item">A</li>
+    <li class="letter-list-item">A</li>
+    <li class="letter-list-item">A</li>
+    <li class="letter-list-item">A</li>
+    <li class="letter-list-item">A</li>
+    <li class="letter-list-item">A</li>
+    <li class="letter-list-item">A</li>
+    <li class="letter-list-item">A</li>
+    <li class="letter-list-item">A</li>
+    <li class="letter-list-item">A</li>
+  </ul> 
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .letter-list
+    display flex
+    flex-direction column
+    justify-content center
+    position absolute
+    right 0
+    top 0
+    bottom 0
+    width .4rem
+    .letter-list-item
+      line-height .4rem
+      text-align center
+      color $bgColor
+</style>
