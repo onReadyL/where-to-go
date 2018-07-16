a1243d4596d5dfb12385c9d43743c4eb8d18cdb6
onReadyL <1169959173@qq.com>
2018-07-16 23:38:45 +0800

    加入缓存以及缓存时的生命周期函数

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
-    <!-- 显示当前 路由地址 对应的内容 -->
-    <router-view></router-view>
+    <!-- 路由被加载后放入内存中 -->
+    <keep-alive>
+      <!-- 显示当前 路由地址 对应的内容 -->
+      <router-view></router-view>
+    </keep-alive>
   </div>
 </template>
 
